123dcf897c4a47c31c65fe1becdaa8a70df73e39
KAlanizK <karinaalaniz1994@gmail.com>
2020-12-17 11:02:58 -0300

    Login

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,12 @@
-var nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const mailConfig = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'nickolas.lind33@ethereal.email',
-        pass: '9XbHM5xvsZppyXKyu4'
-    }
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+      user: 'anibal26@ethereal.email',
+      pass: '3Fy2WHKGKKARWRvB75'
+  }
 });
 
 module.exports = nodemailer.createTransport(mailConfig)
