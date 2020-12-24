63095188564c4de104efeedf523fc89acca64892
KAlanizK <karinaalaniz1994@gmail.com>
2020-12-24 12:17:13 -0300

    privacy_policy.html

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -32,3 +32,11 @@ if (process.env.NODE_ENV == 'production') {
   }
 
 module.exports = nodemailer.createTransport(mailConfig);
+
+/* todo lo que fuimos haciendo: creamos nuestro servidor en Heroku, 
+pusimos la librería "dot-env" para mapear las variables de ambiente 
+al ambiente en el que estemos trabajando, creamos la cuenta Mongo Atlas 
+para tener nuestro servidor en la nube y hemos configurado las variables de 
+ambiente en Heroku para que la URL de Mongo le pegue al servicio de Mongo que 
+creamos con Mongo Atlas. Y ahora, esto último que hicimos, fue crear una cuenta
+en SendGrid para el manejo de mails en ambiente productivo. */
