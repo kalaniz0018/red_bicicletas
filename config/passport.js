7c5e5745c297732e6ba8accaee76d30085935198
KAlanizK <karinaalaniz1994@gmail.com>
2020-12-25 23:54:04 -0300

    Facebook

--- config/passport.js
+++ config/passport.js
@@ -1,19 +1,19 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const Usuario = require('../models/usuario');
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookTokenStrategy = require('passport-facebook-token').Strategy;
 
 passport.use(new LocalStrategy(
-    function (email, password, done){
+    function (email, password, done) {
         //we search the user by his/her email
-        Usuario.findOne({email: email}, (err, usuario)=>{
+        Usuario.findOne({ email: email }, (err, usuario) => {
             //If there is a error
-            if(err) return done(err)
+            if (err) return done(err)
             //If there is not user with taht email
-            if(!usuario) return done(null, false, {message: 'Email no existente o incorrecto'});
+            if (!usuario) return done(null, false, { message: 'Email no existente o incorrecto' });
             //Is password is not valid
-            if (!usuario.validPassword(password)) return done(null, false, {message: 'Password no existente o incorrecto'});
+            if (!usuario.validPassword(password)) return done(null, false, { message: 'Password no existente o incorrecto' });
 
             //Everthing is OK. Execute the callaback
             return done(null, usuario)
@@ -22,26 +22,46 @@ passport.use(new LocalStrategy(
 ));
 
 
+/*passport.use(new FacebookTokenStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+}, function (accesToken, refreshToken, profile, done) {
+    try {
+        user.findOneOrCreateByFacebook(profile, function (err, user) {
+            if (err) console.log('err' + err);
+            return done(err, user);
+        });
+    } catch (err2) {
+        console.log(err2);
+        return done(err2, null);
+    }
+ }
+));*/
+
+
+
+
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callBackURL: process.env.HOST + '/auth/google/callback'
 },
-function(accesToken, refreshToken, profile, cb) {
-    console.log(profile);
-    usuario.findOneOrCreateByGoogle(profile, function(err, user) {
-        return cb(err, user);
-    });
-}
+    function (accesToken, refreshToken, profile, cb) {
+        console.log(profile);
+        usuario.findOneOrCreateByGoogle(profile, function (err, user) {
+            return cb(err, user);
+        });
+    }
 ));
 
 //cb(callback)
-passport.serializeUser(function(user,cb){
+passport.serializeUser(function (user, cb) {
     cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, cb){
-    Usuario.findById(id, function(err, usuario){
+passport.deserializeUser(function (id, cb) {
+    Usuario.findById(id, function (err, usuario) {
         cb(err, usuario);
     });
 });
